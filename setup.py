094f739080090561554b24a753c667b19e9977c2
matao1984 <68136580+matao1984@users.noreply.github.com>
2024-02-26 18:50:22 -0500

    Update setup.py
    Set hyperspy version <2

--- setup.py
+++ setup.py
@@ -139,7 +139,7 @@ setup(
     #
     # For an analysis of "install_requires" vs pip's requirements files see:
     # https://packaging.python.org/en/latest/requirements.html
-    install_requires=['hyperspy>=1.5','scikit-image','pyqt5'],  # Optional
+    install_requires=['hyperspy>=1.5, <1.8','scikit-image','pyqt5'],  # Optional
 
     # List additional groups of dependencies here (e.g. development
     # dependencies). Users will be able to install these using the "extras"
